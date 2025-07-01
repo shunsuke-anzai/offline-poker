61d676882a4884e2a0e12692992b76b20b011862
Yuji Katagi <ktg27wpolo@gmail.com>
2025-07-01 23:29:32 +0900

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,34 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
+    // font-cinzelクラスを適用
     <main className="flex min-h-screen flex-col items-center justify-center bg-black p-4 font-cinzel">
       
       <div className="w-full max-w-xs mx-auto flex flex-col items-center">
 
+        {/* タイトル */}
         <div className="text-center mb-20">
           <h1 className="text-6xl font-bold tracking-widest bg-gold-gradient bg-clip-text text-transparent">
-            MAKE
+            OFFLINE
           </h1>
+          {/* タイトル間の線 */}
           <div className="w-2/3 h-px bg-gold-gradient mx-auto my-4"></div>
           <h1 className="text-6xl font-bold tracking-widest bg-gold-gradient bg-clip-text text-transparent">
-            ROOM
+            POKER
           </h1>
         </div>
 
+        {/* ボタンコンテナ */}
         <div className="w-full flex flex-col space-y-8">
           
+          {/* MAKE ROOM ボタン */}
           <button 
             className="w-full py-4 px-8 text-xl font-bold text-black bg-gold-gradient rounded-full shadow-gold transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-300/50 hover:-translate-y-1 focus:outline-none"
           >
             MAKE ROOM
           </button>
           
+          {/* SEARCH ROOM ボタン */}
           <button 
             className="w-full py-4 px-8 text-xl font-bold text-black bg-gold-gradient rounded-full shadow-gold transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-yellow-300/50 hover:-translate-y-1 focus:outline-none"
           >
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        // "app/layout.tsx" で読み込んだフォントを 'font-cinzel' というクラスで使えるように設定
-        cinzel: ['"Cinzel"', 'serif'],
+        cinzel: ['var(--font-cinzel)'],
       },
       backgroundImage: {
-        // グラデーションの定義を少しシンプルで確実なものに変更
         "gold-gradient": "linear-gradient(145deg, #BF953F, #FCF6BA, #B38728)",
       },
       boxShadow: {
